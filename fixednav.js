3330cd7659dd129c847c2a4ab588166d316781d4
jam <jamesonngo@gmail.com>
2017-08-19 18:16:59 -0400

    ahhh!

---
+++ fixednav.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+	$(window).scroll(function () {
+		//if you hard code, then use console
+		//.log to determine when you want the 
+		//nav bar to stick.  
+		console.log($(window).scrollTop())
+		if ($(window).scrollTop() > $(window).height()) {
+			$('#barr').fadeIn("slow").addClass('barrfixed');
+
+		}
+		if ($(window).scrollTop() < $(window).height()) {
+			$('#barr').fadeOut("slow");
+		}
+	});
+});
