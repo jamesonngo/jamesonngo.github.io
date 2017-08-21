cb6147c5fc9127cd004950f973b176624b039c0f
jam <jamesonngo@gmail.com>
2017-08-21 19:08:04 -0400

    javascript fixednav height response

--- fixednav.js
+++ fixednav.js
@@ -4,12 +4,13 @@ $(document).ready(function () {
 		//if you hard code, then use console
 		//.log to determine when you want the 
 		//nav bar to stick.  
+		var heighth = document.getElementById('header').offsetHeight;
 		console.log($(window).scrollTop())
-		if ($(window).scrollTop() > $(window).height() * 0.95) {
+		if ($(window).scrollTop() > heighth * 0.90) {
 			$('#barr').fadeIn("slow").addClass('barrfixed');
 
 		}
-		if ($(window).scrollTop() < $(window).height() * 0.95) {
+		if ($(window).scrollTop() < heighth * 0.90) {
 			$('#barr').fadeOut("slow");
 		}
 	});
